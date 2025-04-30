b108d15611f527f099095c1ce4ff7c6d16ab6171
Ember 'n0emis' Keske <git@n0emis.eu>
2025-04-30 16:24:02 +0200

    alice: fix parsing of birdwatcher timestamps

--- profiles/alice/default.nix
+++ profiles/alice/default.nix
@@ -13,7 +13,15 @@ in
     systemd.services.alice-lg.restartTriggers = [ config.environment.etc."alice-lg/alice.conf".source ];
     services.alice-lg = {
       enable = true;
-      settings = {
+      settings = let
+        birdwatcherSettings = {
+          type = "single_table";
+          main_table = "master";
+          servertime = "2006-01-02T15:04:05Z07:00";
+          servertime_short = "2006-01-02 15:04:05";
+          servertime_ext = "2006-01-02 15:04:05";
+        };
+      in {
         server = {
           listen_http = "127.0.0.1:7340";
           enable_prefix_lookup = true;
@@ -26,17 +34,13 @@ in
         };
         "source.cactus.birdwatcher" = {
           api = "http://10.120.123.9:29184";
-          type = "single_table";
-          main_table = "master";
-        };
+        } // birdwatcherSettings;
         "source.tumbleweed" = {
           name = "tumbleweed";
         };
         "source.tumbleweed.birdwatcher" = {
           api = "http://10.120.123.10:29184";
-          type = "single_table";
-          main_table = "master";
-        };
+        } // birdwatcherSettings;
       };
     };
 
